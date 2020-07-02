57c4a30e73f70e31275d64611e0dcd5c80fd141c
Hongli Lai <h.lai@fullstaq.com>
2020-07-02 15:55:22 +0200

    Upgrade to vcert-java 0.1.4

--- src/main/java/io/jenkins/plugins/venafivcert/CertRequestBuilder.java
+++ src/main/java/io/jenkins/plugins/venafivcert/CertRequestBuilder.java
@@ -19,7 +19,7 @@ import com.venafi.vcert.sdk.Config.ConfigBuilder;
 import com.venafi.vcert.sdk.certificate.CertificateRequest;
 import com.venafi.vcert.sdk.certificate.KeyType;
 import com.venafi.vcert.sdk.certificate.PEMCollection;
-import com.venafi.vcert.sdk.connectors.tpp.ZoneConfiguration;
+import com.venafi.vcert.sdk.connectors.ZoneConfiguration;
 import com.venafi.vcert.sdk.endpoint.Authentication;
 import com.venafi.vcert.sdk.endpoint.ConnectorType;
 
