91fc6d238be3a162b94b83bd8a1cd83d056bcb79
Hongli Lai <h.lai@fullstaq.com>
2020-07-28 08:39:36 +0200

    CI/CD: attempt to fix tests on Jenkins by adding a dummy test

---
+++ src/test/java/io/jenkins/plugins/venafivcert/CertRequestBuilderTest.java
@@ -0,0 +1,17 @@
+package io.jenkins.plugins.venafivcert;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import static org.junit.Assert.assertTrue;
+
+public class CertRequestBuilderTest {
+    @Rule
+    public JenkinsRule jenkins = new JenkinsRule();
+
+    @Test
+    public void testDummy() throws Exception {
+        assertTrue(true);
+    }
+}
